9185b2ff447307dae36a260af47df19c2430ed56
bmassioui <bouchaib.massioui@gmail.com>
2022-03-08 19:56:02 +0000

    cancel button logic

--- src/app/comments/components/commentForm/commentForm.component.ts
+++ src/app/comments/components/commentForm/commentForm.component.ts
@@ -12,6 +12,7 @@ export class CommentFormComponent implements OnInit {
     @Input() initialText: string = '';
 
     @Output() handleSubmit = new EventEmitter<string>();
+    @Output() handleCancel = new EventEmitter<void>();
 
     public form! : FormGroup;
 
